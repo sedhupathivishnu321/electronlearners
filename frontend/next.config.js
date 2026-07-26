23ea7df6fec2256a4b4cf77cb2ca5b6ea7c7b745
sedhupathivishnu321 <sedhupathivishnu2@gmail.com>
2026-07-26 11:49:07 +0530

    Configure subpath basePath for GitHub Pages and add .nojekyll

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let basePath = '';
+let assetPrefix = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  basePath = `/${repo}`;
+  assetPrefix = `/${repo}/`;
+}
+
 const nextConfig = {
   output: 'export',
+  basePath: basePath,
+  assetPrefix: assetPrefix,
   images: {
     unoptimized: true,
   },
